83d528d1c390786a41b0eb2e5cb0bee933d3966f
hamzeasadi <hamzeasadi1@gmail.com>
2022-06-25 18:15:04 +0100

    feed was added

---
+++ feed.py
@@ -0,0 +1,8 @@
+# import the necessary packages
+from imutils.video import VideoStream
+import imagezmq
+import argparse
+import socket
+import time
+
+
--- feedRecording.py
+++ feedRecording.py
@@ -1,17 +1,31 @@
 import os, sys
+import socket
 from imutils import build_montages
 from datetime import datetime
 import numpy as np
 import imagezmq
 import argparse, imutils, cv2
+from matplotlib import pyplot as plt
 
+# frame_dim = [width, hight]
+frame_dim = [1280, 1024]
 
+imageHub = imagezmq.ImageHub(open_port='tcp://*:8081')
 
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 
 def main():
-    pass
-
+    s.bind(('', '8081'))
+    print('Socket bind complete')
+    s.listen(10)
+    print('Socket now listening')
+    conn,addr=s.accept()
+    while True:
+        (_, frames) = imageHub.recv_image()
+        cv2.imshow(frames)
+        plt.show()
+        break
 
 if __name__ == '__main__':
     main()
